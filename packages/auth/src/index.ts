d618b3fea64040fda85d20d62de1c2fc4ffda7a9
Ricardo Colares <ricardo.colares@hp.com>
2025-03-11 11:06:55 -0300

    permissions with condition

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -40,6 +40,10 @@ export function defineAbilitiesFor(user: User) {
 
   permissions[user.role](user, builder)
 
-  const ability = builder.build()
+  const ability = builder.build({
+    detectSubjectType: (subject) => {
+      return subject.__typename
+    },
+  })
   return ability
 }
