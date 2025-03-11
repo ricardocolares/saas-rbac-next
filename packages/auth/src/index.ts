a6791d7e4dbe8377adc68293c1e3fb9e9c3a72fa
Ricardo Colares <ricardo.colares@hp.com>
2025-03-11 10:54:29 -0300

    improving permissions

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,45 @@
+import {
+  createMongoAbility,
+  CreateAbility,
+  MongoAbility,
+  AbilityBuilder,
+} from '@casl/ability'
+
+import { User } from './models/user'
+import { permissions } from './permissions'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import { z } from 'zod'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
+
+// type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilitiesFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Invalid role: ${user.role}`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+  return ability
+}
